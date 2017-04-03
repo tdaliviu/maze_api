dc96832c3e7e196c15bfdfcc9fff540103298e2a
Liviu Nastasenko <tdaliviu@gmail.com>
2017-04-03 09:13:08 +0300

    API docs

--- maze_eval/urls.py
+++ maze_eval/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import RedirectView
+from rest_framework.documentation import include_docs_urls
 
-from api import urls
+from api import urls as api_urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^api/', include(urls.urlpatterns)),
+    url(r'^api/', include(api_urls.urlpatterns)),
+    url(r'^docs/', include_docs_urls(title='maze_eval API')),
+    url(r'^.*$', RedirectView.as_view(url='/docs/', permanent=False)),
 ]
